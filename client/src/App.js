6cdc1035941cc6ea66e7f78ca13d0a1093e95b5a
Dipit Saha <sdipit099@gmail.com>
2024-08-12 20:41:43 +0600

    push

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import {Route,Routes} from 'react-router-dom';
 import Home from './components/home/Home';
 
 function App() {
